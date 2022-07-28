d5625647a5e80e34fe03a5125a381c3ce59a6120
Valdir Silva <valdipiresba@gmail.com>
2022-07-27 23:49:34 -0300

    add cors no backend para acesso via frontend

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import router from './routes/restaurant.route';
 import routerUser from './routes/user.route';
@@ -5,7 +6,9 @@ import routerUser from './routes/user.route';
 const app = express();
 const port = 8888;
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(routerUser);
 
